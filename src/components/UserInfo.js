112618c0e405c8a36738daad647d214a5b73279c
Umbra <icofundss@gmail.com>
2022-06-20 23:54:15 +0300

    refactor: изменить код UserInfo

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -2,21 +2,30 @@ class UserInfo {
   constructor(selectorProfile) {
     this._profileName = document.querySelector(selectorProfile.name);
     this._profileDescription = document.querySelector(selectorProfile.description);
+    //this._profileAvatar = '';
+    // ! нужен селектор на аватар в профиле. objsData.serverAvatar
   }
 
-  getUserInfo() {
+  getUserInfo(objsData) {
     // Возвращает значения записаные в профиле
+    // ! нужен this._profileAvatar. objsData.serverAvatar
+    //console.log(objsData.serverName);
+
     const profileResult = {
-      name: this._profileName.textContent,
-      description: this._profileDescription.textContent
+      name: this._profileName.textContent = objsData.serverName,
+      description: this._profileDescription.textContent = objsData.serverJob,
+      avatar: ''
     }
     return profileResult;
   }
 
+  // ! Тут данные уйдут на сервер.
   setUserInfo(inputProfileName, inputProfileDescription) {
     // запись нового текста в профиль из полей ввода
-    this._profileName.textContent = inputProfileName.value;
-    this._profileDescription.textContent = inputProfileDescription.value;
+    const a = this._profileName.textContent = inputProfileName.value;
+    const b = this._profileDescription.textContent = inputProfileDescription.value;
+    const obj = { name: a, job: b }
+    return obj;
   }
 }
 
