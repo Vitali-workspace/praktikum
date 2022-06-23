89da1796a4ed0b0334a15f1f4e8b99ca6c8d7249
Umbra <icofundss@gmail.com>
2022-06-23 16:04:32 +0300

    refactor: удалить не нужные константы

--- src/utils/constans.js
+++ src/utils/constans.js
@@ -1,30 +1,3 @@
-const initialCards = [
-  {
-    name: 'Сиди-Бель-Аббес',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-  },
-  {
-    name: 'Вашингтон',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-  },
-  {
-    name: 'Луанда',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-  },
-  {
-    name: 'Соломоновы Острова',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-  },
-  {
-    name: 'Шанхай',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-  },
-  {
-    name: 'Тегеран',
-    link: 'https://cdn.pixabay.com/photo/2022/02/17/07/51/church-7018154_960_720.jpg'
-  }
-];
-
 const container = document.querySelector('.root');
 const profileButtonEdit = container.querySelector('.profile__btn-edit');
 const profileButtonAdd = container.querySelector('.profile__btn-add');
@@ -66,7 +39,6 @@ const configApi = {
 }
 
 export {
-  initialCards,
   profileButtonEdit,
   profileButtonAdd,
   inputName,
